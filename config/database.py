04f344e0d58901451d28cf4e275e353eb800a49d
Angelo Nicolas Wolhein <angelonicolasw@gmail.com>
2023-04-10 14:09:00 -0300

    agregar host para docker

--- config/database.py
+++ config/database.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
-
+import os
 db = None
-
+mongo_host = os.environ.get('MONGO_HOST', 'localhost')
 async def get_database():
     global db
    
@@ -10,7 +10,7 @@ async def get_database():
 
     try:
         # Create a new database connection
-        client = MongoClient("mongodb://localhost:27017/")
+        client = MongoClient(f'mongodb://{mongo_host}:27017/')
         db = client["sanic"]
     except Exception as e:
         print(f"Fallo al establecer conexion con la BD: {e}")
